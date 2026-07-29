51510063e000f44eb180a1b64070648852143fe8
Devendra Dangi <devendradangi824@gmail.com>
2026-07-29 15:35:24 +0530

    for backend

--- backend/src/modules/organization/index.js
+++ backend/src/modules/organization/index.js
@@ -1,5 +1,5 @@
 import organizationRouter from './routes/organization.routes.js';
-export { OrganizationRepository } from './repositories/organizationRepository.js';
+export { OrganizationRepository } from './repositories/OrganizationRepository.js';
 export { OrganizationService } from './services/organization.service.js';
 export { OrganizationController } from './controllers/organization.controller.js';
 export { ORGANIZATION_PERMISSIONS } from './permissions/organization.permission.js';
